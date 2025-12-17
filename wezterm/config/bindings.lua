60ebf564aba4dea9f15bb27220c822147ba60667
ohmycmake <ohmycmake@gmail.com>
2025-12-17 12:09:55 +0800

    modify: change alt + t to ctrl + shifr + t.
    Signed-off-by: ohmycmake <ohmycmake@gmail.com>

--- wezterm/config/bindings.lua
+++ wezterm/config/bindings.lua
@@ -62,7 +62,7 @@ local keys = {
 
    -- tabs --
    -- tabs: spawn+close
-   { key = 't',          mods = mod.SUPER,     action = act.SpawnTab('DefaultDomain') },
+   { key = 't',          mods = 'CTRL|SHIFT',  action = act.SpawnTab('DefaultDomain') },
    { key = 't',          mods = mod.SUPER_REV, action = act.SpawnTab({ DomainName = 'wsl:ubuntu-fish' }) },
    { key = 'w',          mods = mod.SUPER_REV, action = act.CloseCurrentTab({ confirm = false }) },
 
